f7a6e0700f8a08848fee3700b66389422b2d69d1
Beaux <daymon.allen@gmail.com>
2018-01-01 10:19:29 -0600

    first publish
    initialized project

---
+++ src/components/navigationUi.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavigationPrimary extends React.Component {
+  render() {
+    return (
+      <h2>
+        My Application!
+      </h2>
+    );
+  }
+}
+
+export default NavigationPrimary;
